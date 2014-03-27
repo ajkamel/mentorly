da5bdd818a199742bdab6836b26e4927774c8404
Ash Kamel <ajkamel@gmail.com>
2014-03-27 13:49:27 -0400

    Add rails installation for Mentor.ly

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mentorly::Application.config.secret_key_base = '7420dd9ea08d30e560ceadae695480f6e31471d5e50b29858b5fb1fa5d9a72c62a45d8322da95bfd449eef719aa95b5c0e96323b460041b04336a9c8bc36a651'
